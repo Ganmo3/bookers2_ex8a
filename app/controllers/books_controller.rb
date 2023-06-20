810251f08892d085314c337fe58a93337353483e
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-06-20 14:50:00 +0000

    [add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,11 +11,18 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
