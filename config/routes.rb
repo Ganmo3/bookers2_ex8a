48433f6c7148093e72bbdac002646b22c7e8d326
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-06-20 06:47:17 +0000

    [add]book model, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :books, only: [:new, :index, :show, :edit, :destroy, :update]
+
   devise_for :users
   root to: 'homes#top'
-  get "/home/about" => "homes#about"
+  get "/home/about" => "homes#about", as: "about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
